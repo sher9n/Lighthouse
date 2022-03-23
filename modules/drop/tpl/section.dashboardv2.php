de7d64b8351b5289d2d76e5820bc5ec06f8bfc9a
Suren Dhanushka <Suren@CreativeSoftware.com>
2022-03-23 17:19:19 +0530

    drops UI start

--- modules/drop/tpl/section.dashboardv2.php
+++ modules/drop/tpl/section.dashboardv2.php
@@ -1,7 +1,70 @@
 <main class="main-wrapper">
-    <div class="container p-0">
+    <div class="container">
         <div class="row">
+            <div class="col-lg-3">
+                <div class="card shadow">
+                    A
+                </div>
+            </div>
         </div>
+        <div class="row justify-content-md-center">
+            <div class="col-lg-5">
+                <div class="card shadow">
+                    <div class="card-body">
+                        <div class="d-flex align-items-center justify-content-center">
+                            <div class="avator-semi border rounded-circle me-6">
+                                <img src="cdn/img/company-dao.png" class="rounded-circle" width="80" height="80">
+                            </div>
+                            <div>
+                                <div class="fs-3 fw-medium text-truncate">Bankless DAO</div>
+                                <div class="text-muted"></div>
+                            </div>
+                        </div>
+                        <div class="fs-5 fw-medium text-center my-17">Bankless DAO is a decentralized community to coordinate and propagate bankless media, culture, and education. Its goal is to drive adoption and awareness of truly bankless money systems like Ethereum.</div>
+                        <div class="fs-1 fw-semibold text-center">20 $BANK</div>
+                        <div class="fw-medium text-center text-muted mt-2">6 days left</div>
+                    </div>
+                    <div class="dash-divider"></div>
+                    <div class="card-body">
+                        <div class="fs-1 fw-semibold text-center text-primary">75% Complete</div>
+                        <div class="fw-medium text-center text-muted mt-2">0x56H7...0Uw6</div>
+                        <div class="row justify-content-md-center mt-12">
+                            <div class="col-md-auto">
+                                <div class="form-check mb-5">
+                                    <input class="form-check-input" type="checkbox" value="" id="ThreeMonth">
+                                    <label class="form-check-label fs-5 fw-medium" for="ThreeMonth">
+                                        HODL 50 $BANK for 3 months
+                                    </label>
+                                </div>
+                                <div class="form-check mb-5">
+                                    <input class="form-check-input" type="checkbox" value="" id="OneMonth">
+                                    <label class="form-check-label fs-5 fw-medium" for="OneMonth">
+                                        Staked 200 $BANK for 1 month
+                                    </label>
+                                </div>
+                                <div class="form-check mb-5">
+                                    <input class="form-check-input" type="checkbox" value="" id="ProvidedBank">
+                                    <label class="form-check-label fs-5 fw-medium" for="ProvidedBank">
+                                        Provided $3,000 in $BANK / $USDC LP
+                                    </label>
+                                </div>
+                                <div class="form-check mb-5">
+                                    <input class="form-check-input" type="checkbox" value="" id="TwoTweets">
+                                    <label class="form-check-label fs-5 fw-medium" for="TwoTweets">
+                                        Mention @BanklessDAO in at least 2 tweets
+                                    </label>
+                                </div>                            
+                            </div>
+                        </div>    
+                    </div>
+                    <div class="dash-divider"></div>
+                    <div class="card-body text-center pt-6">
+                        <div class="fw-semibold">Complete the remaining actions and check back here.</div>
+                        <button type="submit" class="btn btn-primary btn-lg px-13 text-uppercase mt-8">CLAIM AIRDROP</button>
+                    </div>
+                </div>
+            </div>
+        </div>    
     </div>
 </main>
 <?php include_once app_root . '/templates/dash_foot.php'; ?>
--- templates/dash_header.php
+++ templates/dash_header.php
@@ -1,5 +1,5 @@
 <div class="bg-we-peep alert-show">
-    <div class="container">
+    <div class="container-fluid">
         <div class="py-7 d-flex align-items-center">
             <img src="<?php echo app_cdn_path; ?>img/icon-info.svg" class="img-fluid" />
             <div class="ms-3">To use Lighthouse, please switch to the Ethereum network in your wallet.</div>
@@ -7,7 +7,7 @@
     </div>
 </div>
 <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-white two-bg">
-    <div class="container">
+    <div class="container-fluid">
         <div class="dropdown dropdown-mobile-nav d-lg-none">
             <button class="btn btn-link dropdown-toggle mobile-dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
                 <span class="navbar-toggler-icon"></span>
@@ -17,17 +17,41 @@
                 <li><a class="dropdown-item" href="#">Rewards</a></li>
             </ul>
         </div>
-        <div class="text-lg-center w-lg-24">
-            <a class="" href="dashboard">
-                <img src="<?php echo app_cdn_path; ?>img/logo.svg" class="img-fluid" />
-            </a>
-        </div>
-        <div id="user_menu" class="ms-lg-auto dropdown d-none">
-            <a class="nav-link dropdown-toggle mobile-dropdown-toggle d-flex align-items-center pe-0 text-fiord" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                <div class="avator-md d-flex justify-content-center align-items-center rounded-circle border border-black bg-purple"></div>
-                <div class="mx-4 h5 mb-0 d-none d-lg-flex" id="user_address"></div>
-            </a>
-            <ul id="wallet_addresses" class="dropdown-menu dropdown-menu-end list-wallet" aria-labelledby="navbarDropdown"></ul>
+        <div class="row align-items-center w-100">
+            <div class="col-lg-4">
+                <div class="d-flex align-items-center">
+                    <a class="" href="dashboard">
+                        <img src="<?php echo app_cdn_path; ?>img/logo.svg" class="img-fluid" />
+                    </a>
+                    <ul class="navbar-nav top-navbar-nav-ms">
+                        <li class="nav-item">
+                            <a class="nav-link text-uppercase fw-medium active" href="validate-governance">Drops</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-uppercase fw-medium" href="validate-tokens">My Claims</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <input type="text" id="search_coins" class="form-control form-control-search rounded-pill border-0" placeholder="Search for communities...">
+            </div>
+            <div class="col-lg-4">
+                <div class="d-flex align-items-center justify-content-end">
+                    <div class="form-check form-switch form-switch-start">
+                        <label class="form-check-label fs-5 fw-medium" for="UserSwitch">User View</label>
+                        <input class="form-check-input" type="checkbox" id="UserSwitch">                
+                    </div>
+                    <div id="user_menu" class="dropdown d-flex align-items-center">
+                        <div class="h-divider mx-12"></div>
+                        <a class="nav-link dropdown-toggle mobile-dropdown-toggle d-flex align-items-center px-0 text-fiord" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            <div class="avator-md d-flex justify-content-center align-items-center rounded-circle border border-black bg-purple"></div>
+                            <div class="mx-4 h5 mb-0 d-none d-lg-flex" id="user_address"></div>
+                        </a>
+                        <ul id="wallet_addresses" class="dropdown-menu dropdown-menu-end list-wallet" aria-labelledby="navbarDropdown"></ul>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </nav>
