17039c908914245fe8ecf44cb92030a9d74bf987
Senesh Ruwantha <Senesh@CreativeSoftware.com>
2022-10-11 16:11:34 +0530

    conset box alignment fix

--- app/modules/admin/tpl/partial/ntt-consent-bar.php
+++ app/modules/admin/tpl/partial/ntt-consent-bar.php
@@ -2,7 +2,7 @@
     <div class="card shadow mb-12">
         <div class="card-body px-xl-20">
             <div class="d-flex align-items-center">
-                <div class="fs-4 fw-medium">I consent to receiving non-transferrable reputation tokens ($rep<?php echo $__page->ticker; ?>).</div>
+                <div class="fs-4 fw-medium consent-text">I consent to receiving non-transferrable reputation tokens ($rep<?php echo $__page->ticker; ?>).</div>
                 <div class="ms-auto">
                     <button type="button" data-consent="0" id="i_do_not_consent" class="btn btn-white me-2 btn_consent">I do not consent</button>
                     <button type="button" data-consent="1" id="i_consent" class="btn btn-blue-stone btn_consent">I consent</button>
