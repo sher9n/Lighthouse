cb75d552ba9df438c3bcd469ed07881a30f40aa1
Miner 1908 <commit@workerdao.xyz>
2022-10-17 12:33:34 +0530

    qurum modifcation fixes

--- app/modules/admin/tpl/section.admin-stewards.php
+++ app/modules/admin/tpl/section.admin-stewards.php
@@ -27,85 +27,92 @@
                             <div class="mt-26 <?php echo count($__page->quorumProposals) < 1?'d-none':''; ?>" id="quorum_list">
                             <?php
                             if($__page->blockchain == SOLANA){
+
                                 foreach ($__page->quorumProposals as $qid => $proposal){
-                                    $qdata = json_decode($proposal->proposal_data); ?>
-                                    <div class="prop-<?php echo $qid; ?>  d-flex align-items-center justify-content-between mt-4">
-                                        <div>
-                                            <div class="d-flex align-items-center fw-medium">
-                                                <a class="text-blue-stone me-2 text-decoration-none">Modify</a>
-                                                <div class="text-muted">Quorum</div>
-                                            </div>
-                                            <div class="d-flex align-items-center fw-medium text-gray-700">
-                                                <div class="fs-2"><?php echo $qdata->c; ?></div>
-                                                <div class="fs-3">/<?php echo $__page->stewardCount; ?></div>
-                                            </div>
-                                            <?php
-                                            $date_count = Utils::expire_date_count(date("Y-m-d H:i:s"),date('Y-m-d H:i:s',strtotime($proposal->c_at .' +'.($__page->maxVotingTime/86400).' days')));
-                                            if(!is_null($date_count)) {
-                                                ?>
-                                                <div class="d-flex align-items-center text-blue-stone">
-                                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-clock feather-md"><circle cx="12" cy="12" r="10"></circle><polyline points="12 6 12 12 16 14"></polyline></svg>
-                                                    <div class="fw-medium ms-2 end_time_<?php echo $qid; ?>">Approval period ends in <?php echo $date_count; ?></div>
-                                                </div>
-                                            <?php
-                                            }
-                                            else{
-                                                ?>
-                                                <script>
 
-                                                    var countDownDate_<?php echo $qid; ?> = new Date("<?php echo date('Y-m-d H:i:s',strtotime($proposal->c_at .' +'.($__page->maxVotingTime/86400).' days')); ?>").getTime();
+                                    if(strlen($proposal->proposal_state) > 0) {
 
-                                                    var x_<?php echo $qid; ?> = setInterval(function() {
-
-                                                        var now_<?php echo $qid; ?> = new Date().getTime();
-                                                        var distance_<?php echo $qid; ?> = countDownDate_<?php echo $qid; ?> - now_<?php echo $qid; ?>;
-                                                        var hours_<?php echo $qid; ?> = Math.floor((distance_<?php echo $qid; ?> % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-                                                        var minutes_<?php echo $qid; ?> = Math.floor((distance_<?php echo $qid; ?> % (1000 * 60 * 60)) / (1000 * 60));
-                                                        var seconds_<?php echo $qid; ?> = Math.floor((distance_<?php echo $qid; ?> % (1000 * 60)) / 1000);
-
-                                                        if (countDownDate_<?php echo $qid; ?> < now_<?php echo $qid; ?>) {
-                                                            clearInterval(x_<?php echo $qid; ?>);
-                                                            $(".end_time_<?php echo $qid; ?>").html("EXPIRED");
-                                                        }
-                                                        else {
-                                                            $(".end_time_<?php echo $qid; ?>").html("Approval period ends in " + hours_<?php echo $qid; ?> + "h "
-                                                                + minutes_<?php echo $qid; ?> + "m " + seconds_<?php echo $qid; ?> + "s ");
-                                                        }
-                                                    }, 1000);
-                                                </script>
-                                                <div class="d-flex align-items-center text-blue-stone">
-                                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-clock feather-md"><circle cx="12" cy="12" r="10"></circle><polyline points="12 6 12 12 16 14"></polyline></svg>
-                                                    <div class="fw-medium ms-2 end_time_<?php echo $qid; ?>"></div>
-                                                </div>
-                                                <?php
-                                            } ?>
-                                        </div>
-                                        <div class="text-end">
-                                            <div class="fw-medium text-muted mb-1"><?php echo $proposal->proposal_yes_count; ?> of <?php echo $__page->stewardCount; ?> Approved</div>
-                                            <?php if($__page->is_admin != false){ ?>
+                                        $qdata = json_decode($proposal->proposal_data);
+                                        ?>
+                                        <div class="prop-<?php echo $qid; ?>  d-flex align-items-center justify-content-between mt-4">
                                             <div>
+                                                <div class="d-flex align-items-center fw-medium">
+                                                    <a class="text-blue-stone me-2 text-decoration-none">Modify</a>
+                                                    <div class="text-muted">Quorum</div>
+                                                </div>
+                                                <div class="d-flex align-items-center fw-medium text-gray-700">
+                                                    <div class="fs-2"><?php echo $qdata->c; ?></div>
+                                                    <div class="fs-3">/<?php echo $__page->stewardCount; ?></div>
+                                                </div>
                                                 <?php
-
-                                                if(!isset($__page->user_votes[$qid])){ ?>
-                                                    <a type="button" data-pid="<?php echo $qid; ?>" data-vote="NO" id="deny_<?php echo $qid; ?>" class="admin_proposal_vote btn btn-secondary me-2">Deny</a>
-                                                    <a type="button" data-pid="<?php echo $qid; ?>" data-vote="YES" id="approve_<?php echo $qid; ?>" class="admin_proposal_vote btn btn-blue-stone">Approve</a>
-                                                    <?php
+                                                $date_count = Utils::expire_date_count(date("Y-m-d H:i:s"),date('Y-m-d H:i:s',strtotime($proposal->c_at .' +'.($__page->maxVotingTime/86400).' days')));
+                                                if(!is_null($date_count)) {
+                                                    ?>
+                                                    <div class="d-flex align-items-center text-blue-stone">
+                                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-clock feather-md"><circle cx="12" cy="12" r="10"></circle><polyline points="12 6 12 12 16 14"></polyline></svg>
+                                                        <div class="fw-medium ms-2 end_time_<?php echo $qid; ?>">Approval period ends in <?php echo $date_count; ?></div>
+                                                    </div>
+                                                <?php
                                                 }
-                                                if($proposal->proposal_state == \lighthouse\Proposal::PROPOSAL_STATE_SUCCEEDED ){ ?>
-                                                    <a type="button" data-pid="<?php echo $qid; ?>" id="execute_<?php echo $qid; ?>" class="quorum_proposal_execute btn btn-blue-stone">execute</a>
+                                                else{
+                                                    ?>
+                                                    <script>
+
+                                                        var countDownDate_<?php echo $qid; ?> = new Date("<?php echo date('Y-m-d H:i:s',strtotime($proposal->c_at .' +'.($__page->maxVotingTime/86400).' days')); ?>").getTime();
+
+                                                        var x_<?php echo $qid; ?> = setInterval(function() {
+
+                                                            var now_<?php echo $qid; ?> = new Date().getTime();
+                                                            var distance_<?php echo $qid; ?> = countDownDate_<?php echo $qid; ?> - now_<?php echo $qid; ?>;
+                                                            var hours_<?php echo $qid; ?> = Math.floor((distance_<?php echo $qid; ?> % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+                                                            var minutes_<?php echo $qid; ?> = Math.floor((distance_<?php echo $qid; ?> % (1000 * 60 * 60)) / (1000 * 60));
+                                                            var seconds_<?php echo $qid; ?> = Math.floor((distance_<?php echo $qid; ?> % (1000 * 60)) / 1000);
+
+                                                            if (countDownDate_<?php echo $qid; ?> < now_<?php echo $qid; ?>) {
+                                                                clearInterval(x_<?php echo $qid; ?>);
+                                                                $(".end_time_<?php echo $qid; ?>").html("EXPIRED");
+                                                            }
+                                                            else {
+                                                                $(".end_time_<?php echo $qid; ?>").html("Approval period ends in " + hours_<?php echo $qid; ?> + "h "
+                                                                    + minutes_<?php echo $qid; ?> + "m " + seconds_<?php echo $qid; ?> + "s ");
+                                                            }
+                                                        }, 1000);
+                                                    </script>
+                                                    <div class="d-flex align-items-center text-blue-stone">
+                                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-clock feather-md"><circle cx="12" cy="12" r="10"></circle><polyline points="12 6 12 12 16 14"></polyline></svg>
+                                                        <div class="fw-medium ms-2 end_time_<?php echo $qid; ?>"></div>
+                                                    </div>
                                                     <?php
                                                 } ?>
                                             </div>
-                                            <?php } ?>
-                                            <div class="d-flex align-items-center justify-content-end mt-2">
-                                                <div class="fw-semibold me-2">View Proposal</div>
-                                                <a target="_blank" href="https://solscan.io/account/<?php echo $proposal->proposal_adr ; ?>?cluster=devnet" class="text-primary">
-                                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-external-link"><path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path><polyline points="15 3 21 3 21 9"></polyline><line x1="10" y1="14" x2="21" y2="3"></line></svg>
-                                                </a>
+                                            <div class="text-end">
+                                                <div class="fw-medium text-muted mb-1"><?php echo $proposal->proposal_yes_count; ?> of <?php echo $__page->stewardCount; ?> Approved</div>
+                                                <?php if($__page->is_admin != false){ ?>
+                                                <div>
+                                                    <?php
+
+                                                    if(!isset($__page->user_votes[$qid])){ ?>
+                                                        <a type="button" data-pid="<?php echo $qid; ?>" data-vote="NO" id="deny_<?php echo $qid; ?>" class="admin_proposal_vote btn btn-secondary me-2">Deny</a>
+                                                        <a type="button" data-pid="<?php echo $qid; ?>" data-vote="YES" id="approve_<?php echo $qid; ?>" class="admin_proposal_vote btn btn-blue-stone">Approve</a>
+                                                        <?php
+                                                    }
+                                                    if($proposal->proposal_state == \lighthouse\Proposal::PROPOSAL_STATE_SUCCEEDED ){ ?>
+                                                        <a type="button" data-pid="<?php echo $qid; ?>" id="execute_<?php echo $qid; ?>" class="quorum_proposal_execute btn btn-blue-stone">execute</a>
+                                                        <?php
+                                                    } ?>
+                                                </div>
+                                                <?php } ?>
+                                                <div class="d-flex align-items-center justify-content-end mt-2">
+                                                    <div class="fw-semibold me-2">View Proposal</div>
+                                                    <a target="_blank" href="https://solscan.io/account/<?php echo $proposal->proposal_adr ; ?>?cluster=devnet" class="text-primary">
+                                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-external-link"><path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path><polyline points="15 3 21 3 21 9"></polyline><line x1="10" y1="14" x2="21" y2="3"></line></svg>
+                                                    </a>
+                                                </div>
                                             </div>
                                         </div>
-                                    </div>
-                                <?php }
+                                        <?php
+                                    }
+                                }
                             } ?>
                             </div>
                         </div>
